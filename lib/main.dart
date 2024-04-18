5f43301097042412ea867de7e99acf211c97afd2
WereAM <michelle.ambunya@strathmore.edu>
2024-04-18 18:54:16 +0300

    feat: change title michelle

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo with Adala & Hellen',
+      title: 'Flutter Demo with Adala & Hellen & Michelle',
       theme: ThemeData(
         // This is the theme of your application.
         //
